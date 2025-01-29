f1171229043d7b720c611efbe35ca956232aeb5b
Vijay Prakash <prakash.vijay@gmail.com>
2025-01-29 10:52:05 -0500

    Get metrics handler working

--- proxy_handler.py
+++ proxy_handler.py
@@ -1,24 +1,26 @@
 import http.client
 import http.server
+import json
 
 
 class ProxyHandler(http.server.BaseHTTPRequestHandler):
-    BUFFER_SIZE = 4096
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.protocol_version = "HTTP/1.0"
+    bandwidth_usage_bytes = 0
+    visited_sites = dict[str, int]()
 
     def do_CONNECT(self):
         pass
 
     def do_GET(self):
-        print(self.protocol_version)
         if self.path == "/metrics":
-            self._handle_metrics()
+            metrics = self._get_metrics()
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(metrics).encode())
             return
 
-        http_connection = http.client.HTTPConnection(self.headers["Host"])
+        host = self.headers["Host"]
+        http_connection = http.client.HTTPConnection(host)
         http_connection.putrequest(self.command, self.path, skip_host=True)
         for key, value in self.headers.items():
             http_connection.putheader(key, value)
@@ -33,32 +35,29 @@ class ProxyHandler(http.server.BaseHTTPRequestHandler):
                 self.send_header(key, value)
         self.end_headers()
 
-        self.wfile.write(http_response.read())
+        data = http_response.read()
+        self.wfile.write(data)
+        ProxyHandler.bandwidth_usage_bytes += len(data)
+        ProxyHandler.visited_sites[host] = ProxyHandler.visited_sites.get(host, 0) + 1
 
-    def _handle_metrics(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
-        # self.wfile.write(json.dumps())
+    def _get_metrics(self):
+        top_sites = sorted(
+            [
+                {"url": key, "visits": value}
+                for key, value in ProxyHandler.visited_sites.items()
+            ],
+            key=lambda s: s["visits"],
+            reverse=True,
+        )
 
-    def _forward_chunked_response(self, http_response: http.client.HTTPResponse):
-        while True:
-            first_line = http_response.readline()
-            self.wfile.write(first_line + b"\r\n")
-            chunk_size_str = first_line.strip()
-            try:
-                chunk_size = int(chunk_size_str, 16)  # Convert hex to int
-                print(chunk_size)
-            except ValueError:
-                self.send_error(400, "Invalid chunk size")
-                return
+        if ProxyHandler.bandwidth_usage_bytes < 1_048_576:
+            bandwidth_usage = f"{ProxyHandler.bandwidth_usage_bytes // 1024}KB"
+        else:
+            bandwidth_usage = f"{ProxyHandler.bandwidth_usage_bytes // 1_048_576}MB"
 
-            if chunk_size == 0:
-                break  # Last chunk
+        metrics = {
+            "bandwidth_usage": bandwidth_usage,
+            "top_sites": top_sites[:5],
+        }
 
-            chunk_data = http_response.read(chunk_size)
-            self.wfile.write(chunk_data + b"\r\n")
-            http_response.read(2)  # Consume the trailing CRLF
-
-        # Write final empty chunk.
-        self.wfile.write(b"\r\n")
+        return metrics
